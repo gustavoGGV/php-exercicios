c46059286016f945b08ff23fa826e44efedc776c
Gustavo <134728517+gustavoGGV@users.noreply.github.com>
2024-11-08 15:36:23 -0300

    Create Combatente.php

---
+++ rpg/modelo/Combatente.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once("Mago.php");
+
+class Combatente extends Mago {
+
+    private int $forcaDeAtaque;
+
+    public function lancarPoder() {
+
+        $poder=$this->getPoder();
+        $forcaTotal=$poder->getForcaTotal($this->forcaDeAtaque);
+
+        return "O mago " . $this->nome . "do tipo combatente, possui força de " . $this->forcaDeAtaque . " e lançou o poder '" . $poder->getDescricao() . "' com força total de " . $forcaTotal . ".\n";
+
+    }
+
+    /**
+     * Get the value of forcaDeAtaque
+     */
+    public function getForcaDeAtaque(): int
+    {
+        return $this->forcaDeAtaque;
+    }
+
+    /**
+     * Set the value of forcaDeAtaque
+     */
+    public function setForcaDeAtaque(int $forcaDeAtaque): self
+    {
+        $this->forcaDeAtaque = $forcaDeAtaque;
+
+        return $this;
+    }
+
+}
