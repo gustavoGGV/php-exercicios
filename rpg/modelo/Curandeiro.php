2258f5e7e4e5a1dd23e46a6da0c2de112b5f9244
Gustavo <134728517+gustavoGGV@users.noreply.github.com>
2024-11-08 15:36:02 -0300

    Create Curandeiro.php

---
+++ rpg/modelo/Curandeiro.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once("Mago.php");
+require_once("Poder.php");
+
+class Curandeiro extends Mago {
+
+    private int $forcaDeCura;
+
+    public function lancarPoder() {
+
+        $poder=$this->getPoder();
+        $forcaTotal=$poder->getForcaTotal($this->forcaDeCura);
+
+        return "O mago " . $this->nome . "do tipo curandeiro, possui força de " . $this->forcaDeCura . " e lançou o poder '" . $poder->getDescricao() . "' com força total de " . $forcaTotal . ".\n";
+
+    }
+
+    /**
+     * Get the value of forcaDeCura
+     */
+    public function getForcaDeCura(): int
+    {
+        return $this->forcaDeCura;
+    }
+
+    /**
+     * Set the value of forcaDeCura
+     */
+    public function setForcaDeCura(int $forcaDeCura): self
+    {
+        $this->forcaDeCura = $forcaDeCura;
+
+        return $this;
+    }
+
+}
