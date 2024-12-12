4c2b4bf114d9920d14a4a8d7b12eef132c32313f
Gustavo <134728517+gustavoGGV@users.noreply.github.com>
2024-12-12 14:43:50 -0300

    Create Calculadora.php

---
+++ matematica/modelo/Calculadora.php
@@ -0,0 +1,46 @@
+<?php
+
+abstract class Calculadora {
+
+    protected float $numA;
+    protected float $numB;
+
+    abstract function calcular();
+
+    /**
+     * Get the value of numA
+     */
+    public function getNumA(): float
+    {
+        return $this->numA;
+    }
+
+    /**
+     * Set the value of numA
+     */
+    public function setNumA(float $numA): self
+    {
+        $this->numA = $numA;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of numB
+     */
+    public function getNumB(): float
+    {
+        return $this->numB;
+    }
+
+    /**
+     * Set the value of numB
+     */
+    public function setNumB(float $numB): self
+    {
+        $this->numB = $numB;
+
+        return $this;
+    }
+
+}
